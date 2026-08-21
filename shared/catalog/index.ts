789f79e4bccb53e70b936a3848cb0c630c5bb2ae
dark371star <3.19544584e+08+dark371star@users.noreply.github.com>
2026-08-21 21:07:38 +0000

    feat: add second invertebrate catalog batch

--- shared/catalog/index.ts
+++ shared/catalog/index.ts
@@ -5,9 +5,10 @@ import { reptiles01 } from "./batches/reptiles-01";
 import { amphibians01 } from "./batches/amphibians-01";
 import { fish01 } from "./batches/fish-01";
 import { invertebrates01 } from "./batches/invertebrates-01";
+import { invertebrates02 } from "./batches/invertebrates-02";
 import { validateCatalogBatches, type CatalogBatch } from "./types";
 
-export const catalogBatches: CatalogBatch[] = [mammals01, birds01, reptiles01, amphibians01, fish01, invertebrates01];
+export const catalogBatches: CatalogBatch[] = [mammals01, birds01, reptiles01, amphibians01, fish01, invertebrates01, invertebrates02];
 export const catalogSpecies: Species[] = catalogBatches.flatMap((batch) => batch.species);
 export const catalogValidationErrors = validateCatalogBatches(catalogBatches);
 
